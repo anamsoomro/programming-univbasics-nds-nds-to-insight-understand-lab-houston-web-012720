a48449b0a7fe4f33f67ab9b12280ea08fd40157d
anam soomro <anamsoomroed@gmail.com>
2020-01-01 16:39:18 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,11 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-
+  column_index = 0
+  while column_index < directors_database[0][:movies].length do
+    puts directors_database[0][:movies][column_index][:title]
+    column_index += 1
+  end  
 end
 
 
